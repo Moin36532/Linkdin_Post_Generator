b248f90b5824b3e2d455cfaf276913ece73eede2
Moin36532 <moinqammar325@gmail.com>
2025-09-18 17:15:16 +0500

    Update Linkdin_post_generator.py

--- Linkdin_post_generator.py
+++ Linkdin_post_generator.py
@@ -4,7 +4,7 @@ import streamlit as st
 import os
 from langchain_core.prompts import PromptTemplate
 load_dotenv()
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 import streamlit as st
 # hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
@@ -94,6 +94,7 @@ if st.button("Generate Post"):
     st.write("### Generated Linkdin Post:")
 
     st.write(result.content)
+
 
 
 
